df55b57a6ec6fb036cff70d379b6c4791f56c8b7
Owen Buckley <owenbuckley13@gmail.com>
2024-05-23 10:24:21 -0400

    fix DSD usage for greeting component

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -1,14 +1,15 @@
 export class Greeting extends HTMLElement {
   connectedCallback() {
     if(!this.shadowRoot) {
+      const template = document.createElement('template');
       const greeting = this.hasAttribute('name')
         ? this.getAttribute('name')
         : 'World';
-      
+
+      template.innerHTML = `<h2>Hello ${greeting}! 👋</h2>`;
+
       this.attachShadow({ mode: 'open' });
-      this.shadowRoot.innerHTML = `
-        <h2>Hello ${greeting}! 👋</h2>
-      `
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
   }
 }
